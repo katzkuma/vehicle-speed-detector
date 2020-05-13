84ea899810c370b662d8802e1fe8067c7a2d11ca
katzkuma <katzkuma@gmail.com>
2020-05-13 14:10:02 -0400

    feat: add camera model and traffic record model into models.py and register model in admin.py

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Camera, TrafficRecord
 
 # Register your models here.
+admin.site.register(Camera)
+admin.site.register(TrafficRecord)
--- website/models.py
+++ website/models.py
@@ -5,15 +5,15 @@ from django.db import models
 class Camera(models.Model):
     camera_name = models.CharField(default="untitled", max_length=50)
     stream_url = models.TextField(default="unsetted")
-    camera_latitude = models.DecimalField
-    camera_longitude = models.DecimalField
-    focal_length = models.FloatField
-    camera_height = models.FloatField
-    region_of_interest = models.TextField(default="((0, 0), (0, 0), (0, 0), (0, 0))")
-    first_lat_recognition_section = models.DecimalField
-    first_lon_recognition_section = models.DecimalField
-    second_lat_recognition_section = models.DecimalField
-    second_lon_recognition_section = models.DecimalField
+    camera_latitude = models.DecimalField(default=0, decimal_places=6, max_digits=9)
+    camera_longitude = models.DecimalField(default=0, decimal_places=6, max_digits=9)
+    focal_length = models.FloatField(default=0)
+    camera_height = models.FloatField(default=0)
+    region_of_interest = models.TextField(default='{ "first_point":[0, 0], "second_point":[0, 0], "third_point":[0, 0], "fourth_point":[0, 0] }')
+    first_lat_recognition_section = models.DecimalField(default=0, decimal_places=6, max_digits=9)
+    first_lon_recognition_section = models.DecimalField(default=0, decimal_places=6, max_digits=9)
+    second_lat_recognition_section = models.DecimalField(default=0, decimal_places=6, max_digits=9)
+    second_lon_recognition_section = models.DecimalField(default=0, decimal_places=6, max_digits=9)
     last_modify_date = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
     enabled = models.BooleanField(default=False)
@@ -22,11 +22,11 @@ class Camera(models.Model):
 
 class TrafficRecord(models.Model):
     created = models.DateTimeField(auto_now_add=True)
-    first_lat_recognition_section = models.DecimalField
-    first_lon_recognition_section = models.DecimalField
-    second_lat_recognition_section = models.DecimalField
-    second_lon_recognition_section = models.DecimalField
-    detected_vehicles = models.IntegerField
-    time_mean_speed = models.FloatField
+    first_lat_recognition_section = models.DecimalField(default=0, decimal_places=6, max_digits=9)
+    first_lon_recognition_section = models.DecimalField(default=0, decimal_places=6, max_digits=9)
+    second_lat_recognition_section = models.DecimalField(default=0, decimal_places=6, max_digits=9)
+    second_lon_recognition_section = models.DecimalField(default=0, decimal_places=6, max_digits=9)
+    detected_vehicles = models.IntegerField(default=0)
+    time_mean_speed = models.FloatField(default=0)
     class Meta:
         db_table = "tsd_traffic_history"
