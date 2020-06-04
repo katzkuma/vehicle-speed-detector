4a1f82ea3a95fc7805e38e845f947cea966b0168
katzkuma <katzkuma@gmail.com>
2020-06-04 09:35:10 -0400

    fix: fix the problem of streamming error by modifying videoStream.py
    舊版的影像串流類別的執行邏輯錯誤，照成神經網路使用錯誤的影像來源進行判斷，
    新版已將執行邏輯修改為以下步驟：
    1. 首先建立一條主執行緒（Mutiple_Streammer_Thread）
    2. 由此執行緒針對每一台要連線的攝影機建立各別的子執行緒取連接並取得影像
    3. 再由主執行緒彙整所有來源的影像於字典集合裡提供其他類別取用:wq

--- vehicle_detector/vehicle_recognition/camera/videoStream.py
+++ vehicle_detector/vehicle_recognition/camera/videoStream.py
@@ -2,66 +2,58 @@
 from threading import Thread
 import datetime
 from cv2 import cv2
+from imutils.video import WebcamVideoStream
+from website.models import URLPathByBrand 
 
 class VideoStream():
-	def __init__(self, src=None, name="WebcamVideoStream"):
-		# initialize the video camera stream and read the first frame
-		# from the stream
-		if src != None:
-			self.stream = cv2.VideoCapture(src)
-			(self.grabbed, self.frame) = self.stream.read()
+    def __init__(self, cameraSet=None):
+        # initialize the dict for streamming objects
+        self.cameras = dict()
+        # initialize the dict for getting new frame
+        self.frames = dict()
+        # initialize the thread name
+        self.name = 'Mutiple_Streammer_Thread'
+        # initialize the variable used to indicate if the thread should
+        # be stopped
+        self.stopped = False
 
-		# initialize the thread name
-		self.name = name
+        # initialize the video camera stream and read the first frame
+        # from the stream
+        if cameraSet is not None:
+            for camera in cameraSet:
+                url_path = URLPathByBrand.objects.get(camera_brand=camera.camera_brand, streamming_type=camera.streamming_type).URLPath
+                
+                # create streamming_url from data
+                # the MAC is for the webcam of macbook 
+                if camera.camera_brand == 'MAC':
+                    streamming_url = 0
+                else:
+                    if camera.streamming_type == "IMAGE":
+                        streamming_url = 'http://' + camera.camera_user + ':' + camera.camera_password + '@' + camera.ip_address + url_path
+                    else:
+                        streamming_url = 'rtsp://' + camera.camera_user + ':' + camera.camera_password + '@' + camera.ip_address + url_path
+                self.cameras[camera.camera_name] = WebcamVideoStream(streamming_url, camera.camera_name).start()
+    def start(self):
+        # start the thread to read frames from the video stream
+        t = Thread(target=self.update, name=self.name, args=())
+        t.daemon = True
+        t.start()
+        return self
 
-		# initialize url of streamming
-		self.url = src
+    def update(self):
+        # keep looping infinitely until the thread is stopped
+        while True:
+            if self.stopped:
+                for camera_name in self.cameras:
+                    self.cameras[camera_name].stop()
+                break
+            else:
+                for camera_name in self.cameras:
+                    self.frames[camera_name] = self.cameras[camera_name].read()
 
-		# initialize a frame
-		self.frame = None
+    def read(self):
+        return self.frames 
 
-		# initialize the variable used to indicate if the thread should
-		# be stopped
-		self.stopped = True
-
-	def start(self):
-		self.stopped = False
-		# start the thread to read frames from the video stream
-		t = Thread(target=self.update, name=self.name, args=())
-		t.daemon = True
-		t.start()
-		return self
-
-	def update(self):
-		# keep looping infinitely until the thread is stopped
-		while True:
-			# if the thread indicator variable is set, stop the thread
-			if self.stopped:
-				self.stream = cv2.VideoCapture(None)
-				return
-			
-			if self.url is not None:
-				self.stream = cv2.VideoCapture(self.url)
-				# otherwise, read the next frame from the stream
-				(self.grabbed, self.frame) = self.stream.read()
-
-	def read(self):
-		start_time = datetime.datetime.now()
-		while (datetime.datetime.now() - start_time).total_seconds() < 6 :
-			if self.frame is not None:
-				newFrame = self.frame
-				self.frame =  None
-				# return the frame most recently read
-				return newFrame
-		return None 
-		
-
-	def stop(self):
-		# indicate that the thread should be stopped
-		self.stopped = True
-
-	def source(self, url):
-		self.url = url
-
-	# def isURLNone(self):
-	# 	return True if self.url is None else False
+    def stop(self):
+        # indicate that the thread should be stopped
+        self.stopped = True
